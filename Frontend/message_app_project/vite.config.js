1a4e43c295cac109d46dc9831f68ed38ecd150ac
bbwwgg <jackedixon4@gmail.com>
2026-01-18 20:27:03 +1100

    Feat: Docker completed for front and backend

--- Frontend/message_app_project/vite.config.js
+++ Frontend/message_app_project/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Listen on all network interfaces
+    port: 5173,      // Explicitly define port
+    watch: {
+      usePolling: true, // Needed for Docker
+    },
+    hmr: {
+      host: 'localhost', // HMR host is tricky in Docker
+    },
+  },
+});
