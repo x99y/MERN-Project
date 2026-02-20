ee91cb64ee46ca21827a8e3e73b078a4ec54aed7
bbwwgg <jackedixon4@gmail.com>
2026-02-21 00:18:48 +1100

    DB connection to online host working

--- Backend/src/database/connectionManager.js
+++ Backend/src/database/connectionManager.js
@@ -3,12 +3,14 @@ const { default: mongoose } = require("mongoose");
 
 async function dbConnect() {
 	try {
-		let targetDatabaseUrl = process.env.DATABASE_URL || "mongodb://mongo:27017/DatingAppDatabase";
+		let targetDatabaseUrl = process.env.MONGO_URL || "mongodb://mongo:27017/DatingAppDatabase";
 		console.log("Connecting to database: " + targetDatabaseUrl);
 		await mongoose.connect(targetDatabaseUrl);
 		console.log("Database connected!");
 	} catch (error) {
 		console.log("Database connection failed!\n" + JSON.stringify(error));
+		// dont deploy if database connection fails
+		process.exit(1);
 	}
 }
 
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,3 +1,6 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
+
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || 'localhost';
 const { dbConnect } = require("./database/connectionManager.js");
