b65e6e0db176b2883657f663e6690cd1a9f27a3e
bbwwgg <jackedixon4@gmail.com>
2026-02-21 00:27:41 +1100

    Update index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,5 +1,7 @@
-const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
+//const path = require('path');
+//require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
+
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || 'localhost';
