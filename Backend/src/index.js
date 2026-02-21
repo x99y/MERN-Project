607400569b9176cd31d98b2ca08d3dfc217e8c6b
bbwwgg <jackedixon4@gmail.com>
2026-02-22 04:02:23 +1100

    Dockerfile change Test

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,12 +1,7 @@
-console.log("NODE_ENV:", process.env.NODE_ENV);
-console.log("JWT_SECRET:", process.env.JWT_SECRET ? "set" : "undefined");
-console.log("MONGODB_DATABASE_URL:", process.env.MONGODB_DATABASE_URL ? "set" : "undefined");
-console.log("FRONTEND_URL:", process.env.FRONTEND_URL ? "set" : "undefined");
-
 if (process.env.NODE_ENV !== 'production') {
-  const path = require('path');
-  require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
+  require('dotenv').config();
 }
+
 
 const http = require("http");                 
 const { dbConnect } = require("./database/connectionManager.js");
@@ -14,7 +9,7 @@ const { app } = require("./server.js");
 const initSocket = require("./sockets/websocket.js");
 
 const PORT = process.env.PORT || 8000;
-const HOST = process.env.HOST || 'localhost';
+//const HOST = process.env.HOST || 'localhost';
 
 // DATABASE CONNECTION
 dbConnect().then(() => {
