fe7175fe4e89079239c9523967d40328ca1905f9
bbwwgg <jackedixon4@gmail.com>
2026-02-22 02:49:31 +1100

    Backend env testing

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,3 +1,8 @@
+console.log("NODE_ENV:", process.env.NODE_ENV);
+console.log("JWT_SECRET:", process.env.JWT_SECRET ? "set" : "undefined");
+console.log("MONGODB_DATABASE_URL:", process.env.MONGODB_DATABASE_URL ? "set" : "undefined");
+console.log("FRONTEND_URL:", process.env.FRONTEND_URL ? "set" : "undefined");
+
 if (process.env.NODE_ENV !== 'production') {
   const path = require('path');
   require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
@@ -8,7 +13,7 @@ const { dbConnect } = require("./database/connectionManager.js");
 const { app } = require("./server.js");
 const initSocket = require("./sockets/websocket.js");
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 const HOST = process.env.HOST || 'localhost';
 
 // DATABASE CONNECTION
@@ -20,7 +25,7 @@ dbConnect().then(() => {
   initSocket(server);
 
   // Start server
-  server.listen(PORT, "0.0.0.0", () => {
+  server.listen(PORT,"0.0.0.0", () => {
     console.log("Server running on port:", PORT);
   });
 });
